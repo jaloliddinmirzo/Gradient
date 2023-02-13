0aab984aa15899a3ea1ff45c247a8d780eb439bc
Jaloliddin7 <abdutolipovjaloliddin913@gmail.com>
2023-02-13 15:37:00 +0500

    End

--- app.js
+++ app.js
@@ -17,5 +17,6 @@ btn.addEventListener('click' , ()=> {
     let deg = Math.trunc(Math.random() * 360)
     document.body.style.background  =  `linear-gradient(${deg}deg, ${color1} 0%, ${color2} 100%)`
     bcg.style.background = `linear-gradient(${deg}deg, ${color2} 0%, ${color1} 100%)`
+    bcg.style.boxShadow = `6px 6px 0 ${color2}`
     // console.log(deg);
 })
